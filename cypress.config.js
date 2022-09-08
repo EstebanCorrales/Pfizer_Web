20582fb415f1299f1ab69544ba7b41bfc09cd85e
Esteban <105834733+EstebanCorrales@users.noreply.github.com>
2022-09-08 15:58:11 -0600

    aa

--- cypress.config.js
+++ cypress.config.js
@@ -9,7 +9,7 @@ module.exports = defineConfig({
    e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-      //require('cypress-mochawesome-reporter/plugin')(on);
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 
